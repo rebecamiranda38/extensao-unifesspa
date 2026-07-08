12b53d5b47c8a819bfdcaa4b8171aa6372919342
rebecamiranda38 <becamiranda65@gmail.com>
2026-07-08 09:40:53 -0300

    Create AppConfig.java

---
+++ src/main/java/extensao/AppConfig.java
@@ -0,0 +1,18 @@
+package extensao;
+
+public final class AppConfig {
+
+    public static final String DB_HOST = "127.0.0.1";
+    public static final String DB_PORT = "3306";
+    public static final String DB_NAME = "extensao_unifesspa";
+
+    public static final String DB_USER = "root";
+    public static final String DB_PASSWORD = "SUA_SENHA_AQUI";
+
+    public static final String JDBC_URL =
+            "jdbc:mysql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME
+                    + "?useSSL=false&serverTimezone=America/Belem&allowPublicKeyRetrieval=true";
+
+    private AppConfig() {
+    }
+}
